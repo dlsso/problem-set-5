745803c0c17c0f7679729e1ea8a4d416948eac3d
dlsso <supertonesfan@hotmail.com>
2014-07-29 12:16:12 -0600

    Solved problem 2

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ var alphabetSoup = function(string) {
 	for (var i = 0; i < string.length; i++) {
 		lettersArray.push(string[i])
 	}
-	console.log("alphabetized:", alphabetized)
-	console.log("lettersArray:", lettersArray)
 	alphabetized = lettersArray.sort().join('')
-	
+	console.log("alphabetSoup('zxvczabcde')")
 	console.log(alphabetized)
 	return alphabetized
 }
 alphabetSoup("zxvczabcde")
+
+var vowelCount = function(string) {
+	var vowelCounter = 0
+	vowels = { a:0, e:0, i:0, o:0, u:0}
+	for (var i = 0; i < string.length; i++) {
+		if(string[i].toLowerCase() in vowels){
+			vowelCounter++
+		}
+	}
+	console.log('vowelCount("All cows eat grass")')
+	console.log(vowelCounter)
+	return vowelCounter
+}
+vowelCount("All cows eat grass")
